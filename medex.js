2b0111d4f20e84beddabd02e39118f2a15801857
Yonsm <Yonsm@msn.com>
2013-08-28 18:19:49 +0800

    更新支持网页模式

--- medex.js
+++ medex.js
@@ -54,8 +54,8 @@ function medex()
 		}
 	}
 
-	//var nothumb = false;
-	function fetchThumb(thumbs, href, type)
+	var nothumb = false;
+	function fetchThumb(href, type)
 	{
 		// Fetch image
 		var pos = href.lastIndexOf('/');
@@ -70,14 +70,14 @@ function medex()
 				return thumbs[i];
 			}
 		}
-		//if(type != 'dir') nothumb = true;
+		if(type != 'dir') nothumb = true;
 		return type ? ('/appletv/image/' + type + '.png') : href;
 	}
 
 	//
 	var items = '';
 	var items_count = 0;
-	var items_per_row = document.documentElement.clientWidth / 180 | 0;
+	var items_per_row = document.documentElement.clientWidth / 160 | 0;
 	if (items_per_row < 1) items_per_row = 1;
 	function addItems(name, href, type)
 	{
@@ -86,8 +86,9 @@ function medex()
 		{
 			items += '<tr>';
 		}
-		items += '<td width="180" height="180" align="center"><a id="' + type + items_count + '" href="' + href + '">';
-		items += '<image width="134" src=' + fetchThumb(thumbs, href, type) + '></image>';
+		var thumb = (name == '上一级') ? ('/appletv/image/up.png') : fetchThumb(href, type);
+		items += '<td width="160" height="180" align="center"><a id="' + type + items_count + '" href="' + href + '">';
+		items += '<image width="134" src=' + thumb + '></image>';
 		items += '<br>' + name;
 		items += '</a></td>';
 		items_count++;
@@ -111,15 +112,21 @@ function medex()
 		var href = files[i][0];
 		var name = files[i][1];
 		var type = href.substring(href.lastIndexOf('.') + 1).toLowerCase();
-		if(type == 'jpg' || type == 'png' || type == 'gif' || type == 'bmp' || type == 'mov' || type == 'mp4' || type == 'm4v' || type == 'mkv' || type == 'mpg' || type == 'avi' || type == 'm3u' || type == 'mp3' || type == 'aac')
+		if(type != 'jpg' &&
+		 type != 'png' && 
+		 type != 'gif' && 
+		 type != 'bmp' && 
+		 type != 'mov' && 
+		 type != 'mp4' && 
+		 type != 'm4v' && 
+		 type != 'mkv' && 
+		 type != 'mpg' && 
+		 type != 'avi' && 
+		 type != 'm3u' && 
+		 type != 'mp3' && 
+		 type != 'aac')
 		{
-		}
-		else if(type == 'txt' || type == 'doc' || type == 'pdf' || type == 'rtf')
-		{
-			type = 'doc';
-		}
-		else
-		{
+
 			type = 'def';
 		}
 
@@ -130,15 +137,15 @@ function medex()
 	}
 
 	// Request for thumbnail on server
-	/*if(nothumb)
+	if(nothumb)
 	{
-		var uri = url.substring(url.indexOf('/', 8) + 1);
-		var pos = uri.indexOf('?');
-		if(pos != -1) uri = uri.substring(0, pos);
-		pos = uri.indexOf('#');
-		if(pos != -1) uri = uri.substring(0, pos);
-		httpRequest('thumb.cgi?' + uri);
-	}*/
+		var path = location.href;
+		var pos = path.lastIndexOf("//");
+		if(pos != -1) path = path.substring(pos + 2);
+		pos = path.indexOf("/");
+		if(pos != -1) path = path.substring(pos + 1);
+		items += '<tr><td align="center" height="100" colspan="' + items_per_row + '"><a href="/appletv/thumb.cgi?' + path + '">生成缩略图</a></td></tr>';
+	}
 
 	// Geenrate HTML
 	return '<table align="center">' + items + '</table>';
